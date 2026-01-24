14cd1bbece69befb1d517bd70862ee2d8bdc5ed9
stotic-dev <taichis844@gmail.com>
2026-01-24 10:19:13 +0900

    Initial Commit

---
+++ TextViewSample/TextViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextViewSampleApp.swift
+//  TextViewSample
+//
+//  Created by Taichi Sato on 2026/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TextViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
